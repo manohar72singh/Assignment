6cf3009451611daa658b193e9c0b3c5521374e8b
Manohar Kumar Singh <manohar72singh@gmail.com>
2023-02-16 10:36:54 +0530

    string Assignment

---
+++ StringJs/checkString.js
@@ -0,0 +1,15 @@
+checkString=(string, checkStr)=>{
+    // boolean check = true;
+    var flag =0;
+    //var 
+    for(var i=0;i<string.length;i++)
+    {
+        
+        console.log(string[i]);
+    }
+    //console.log(string[i]);
+}
+var string ="My name is Manohar Singh";
+var findString="Manohar";
+var find = checkString(string, findString);
+// console.log(find);
