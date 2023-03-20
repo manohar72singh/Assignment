746590d9b6a2020f454db828b938c02dda7d4544
Manohar Kumar Singh <manohar72singh@gmail.com>
2023-03-20 16:53:27 +0530

    vallidate all page

--- onlaineShopingApp/js/authentication.js
+++ onlaineShopingApp/js/authentication.js
@@ -1,5 +1,85 @@
-// valiidate signIn page
+const database = new Array;
 
+// if email exits
+function emailExits(email)
+{
+    return database.some(function(db){
+        return db.email === email;
+    });
+}
+
+function login(email,password)
+{
+    return database.some(function(db){
+        return db.email === email && db.password === password;
+    });
+}
+
+//validation signIn page
+function validationSignin(){
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var usernameformat = /^(?=.{8,20}$)(?![_.])(?!.*[_.])[a-zA-Z0-9._]+(?<![_.])$/;
+    var passw=  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+    var error = document.querySelector(".error");
+    var username = document.querySelector("#username");
+    var password = document.querySelector("#password");
+    
+    if(username.value.trim()=="" )
+    {
+        error.innerHTML ="Enter username or email First"
+        username.style.border= "1px solid red";
+        username.focus();
+        return;
+    }
+    else if(!(username.value.match(mailformat) || username.value.match(usernameformat)))
+    {
+        error.innerHTML ="Enter Vallid Email or username"
+        username.style.border= "1px solid red";
+        username.focus();
+        return;
+    }
+    else
+    {
+        error.innerHTML =""
+        username.style.border = "1px solid green";
+    }
+    if(password.value.trim()=="")
+    {
+        error.innerHTML = "enter password first";
+        password.style.border = "1px solid red";
+        password.focus();
+        return;
+    }
+    else if(!(password.value.match(passw)))
+    {
+        error.innerHTML = "enter vallid password (Abcde@123)"
+        password.style.border = "1px solid red";
+        password.focus();
+        return;
+    }
+    else{
+        error.innerHTML = ""
+        password.style.border = "1px solid green";
+    }
+    
+    var email = username.value;
+    var password = password.value;
+    console.log(login(email, password));
+    if(login(email, password))
+    {
+        alert("Login Successful");
+        window.localStorage.setItem("email",email);
+        location.assign("home.html");
+    }
+    else{
+        alert("please signup");
+        return;
+    }
+    username.value="";
+    password.value="";
+}
+
+// valiidate signup page
 function validationSignup(){
     var letters = /^[a-zA-Z\s-, ]+$/;
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -11,12 +91,18 @@ function validationSignup(){
     var username = document.querySelector("#username");
     var password = document.querySelector("#password");
     var re_password = document.querySelector('#re-password');
-    alert("hii");
-    
-    if(mobemail.value.trim()=="" )
+
+    if(emailExits(mobemail))
+    {
+        error.innerHTML = "Email already used";
+        mobemail.style.border= "1px solid red";
+        mobemail.focus();
+        return;
+    }
+
+    else if(mobemail.value.trim()=="" )
     {
         error.innerHTML ="Enter Email First"
-        error.style.color= "red";
         mobemail.style.border= "1px solid red";
         mobemail.focus();
         return;
@@ -25,7 +111,6 @@ function validationSignup(){
     else if(!(mobemail.value.match(mailformat)))
     {
         error.innerHTML ="Enter Vallid Email"
-        error.style.color= "red";
         mobemail.style.border= "1px solid red";
         mobemail.focus();
         return;
@@ -33,15 +118,12 @@ function validationSignup(){
     else
     {
         error.innerHTML =""
-        error.style.color= "green";
         mobemail.style.border= "1px solid green";
     }
-    
 
     if(fullname.value.trim()=="")
     {
-        error.innerHTML ="plese Enter Name"
-        error.style.color= "red";
+        error.innerHTML ="Enter Name first"
         fullname.style.border= "1px solid red";
         fullname.focus();
         return;
@@ -49,7 +131,6 @@ function validationSignup(){
     else if(!(fullname.value.match(letters)))
     {
         error.innerHTML ="Enter Vallid name"
-        error.style.color= "red";
         fullname.style.border= "1px solid red";
         fullname.focus();
         return;
@@ -57,7 +138,6 @@ function validationSignup(){
     else if(fullname.value.length < 4 )
     {
         error.innerHTML ="Name Must Be More Than 4 Characters"
-        error.style.color= "red";
         fullname.style.border= "1px solid red";
         fullname.focus();
         return;
@@ -65,42 +145,119 @@ function validationSignup(){
     else
     {
         error.innerHTML =""
-        error.style.color= "green";
         fullname.style.border= "1px solid green";
     }
-
-    
+   
     if(username.value.trim()=="")
     {
-        error.innerHTML ="plese Enter userName"
-        error.style.color= "red";
+        error.innerHTML =" Enter userName first";
         username.style.border= "1px solid red";
         username.focus();
         return;
     }
     else if(!(username.value.match(usernameformat)))
     {
-        error.innerHTML ="Enter Vallid username don't use(. _)"
-        error.style.color= "red";
+        error.innerHTML ="Enter Vallid username don't use special characters";
         username.style.border= "1px solid red";
         username.focus();
         return;
     }
-    // else if(username.value.length < 10 )
-    // {
-    //     error.innerHTML ="Name Must Be More Than 8 Characters"
-    //     error.style.color= "red";
-    //     username.style.border= "1px solid red";
-    //     username.focus();
-    //     return;
-    // }
     else
     {
         error.innerHTML =""
-        error.style.color= "green";
         username.style.border= "1px solid green";
     }
 
+    if(password.value.trim()=="")
+    {
+        error.innerHTML ="Enter password first";
+        password.style.border = "1px solid red";
+        password.focus();
+        return;
+    }
+    else if(!(password.value.match(passw)))
+    {
+        error.innerHTML = "enter vallid password (Abcde@123)";
+        password.style.border = "1px solid red";
+        password.focus();
+        return;
+    }
+    else
+    {
+        error.innerHTML= "";
+        password.style.border = "1px solid green";
+    }
 
-
+    if(re_password.value.trim()=="")
+    {
+        error.innerHTML =" Enter re-password first";
+        re_password.style.border = "1px solid red";
+        re_password.focus();
+        return;
+    }
+    else if(!(password.value === re_password.value))
+    {
+        error.innerHTML = "re-Password Must same as password";
+        re_password.style.border = "1px solid red";
+        re_password.focus();
+        return;
+    }
+    else
+    {
+        error.innerHTML= "";
+        re_password.style.border = "1px solid green";
+    }
+    
+    // set value to database :
+    
+    const obj = {
+        email: mobemail.value,
+        name :fullname.value,
+        username : username.value,
+        password : password.value
+    }
+    database.push(obj);
+     
+    // set input value is empaty
+    mobemail.value="";
+    username.value = "";
+    fullname.value = "";
+    password.value ="";
+    re_password.value ="";
+    console.log(database);
+    alert("signin succesfull");
 }
+//valiidate forgot password
+function validationForgotPassword(){
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var usernameformat = /^(?=.{8,20}$)(?![_.])(?!.*[_.])[a-zA-Z0-9._]+(?<![_.])$/;
+    var username = document.querySelector("#username");
+    var error = document.querySelector(".error");
+
+    if(username.value.trim()=="" )
+    {
+        error.innerHTML ="Enter username or email First"
+        username.style.border= "1px solid red";
+        username.focus();
+        return;
+    }
+    else if(!(username.value.match(mailformat) || username.value.match(usernameformat)))
+    {
+        error.innerHTML ="Enter Vallid Email or username"
+        username.style.border= "1px solid red";
+        username.focus();
+        return;
+    }
+    else
+    {
+        error.innerHTML =""
+        username.style.border = "1px solid green";
+    }
+}
+
+function logout(){
+    alert("logout Successful");
+    window.localStorage.removeItem("email");
+    location.assign("signin.html");
+}
+
